eeb76a277290595f8cef35cd828919af12d07796
Caleb Ontiveros <caleb@Calebs-MacBook-Air.local>
2018-07-18 14:23:01 -0400

    Save

--- app/controllers/frontendi_controller.rb
+++ app/controllers/frontendi_controller.rb
@@ -1,2 +1,7 @@
 class FrontendiController < ApplicationController
+  def root
+    propositions = Proposition.all
+    @proposition_map = {}
+    propositions.each { |el| @proposition_map[el.id] = el.attributes }
+  end
 end
--- app/helpers/frontendi_helper.rb
+++ app/helpers/frontendi_helper.rb
@@ -1,2 +1,7 @@
 module FrontendiHelper
+  def to_json_map(array)
+    map = {}
+    array.each { |el| map[el.id] = el.attributes }
+    map
+  end
 end
